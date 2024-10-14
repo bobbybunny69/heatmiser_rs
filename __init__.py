a36a58b4a1dc433a4ec2c7232c92f53eb73c1212
bobbybunny69 <rob@speckledfrog.com>
2024-10-15 00:29:40 +0100

    Updated vers in __init__

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ v5:  Working version locked to start improvements
 v6:  Various imporvements to robustness and fully async
 v7:  Wrapped port open/close around read/writes to improve connectivty reliability
 TODO: Fix the service schemas to use enttity schema (see HA log - HA will depricate in 2025.9)
+TODO: Sort out the thermo avaiablity (hub down event breaks it)
 """
 from __future__ import annotations
 
